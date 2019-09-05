b33356c500c4ebbe3c7236920a33728dfb7e9f72
Daniel Aaron <danjaaron@gmail.com>
2019-09-04 20:02:47 -0700

    added serial hypertune

---
+++ hypertune.py
@@ -0,0 +1,123 @@
+"""
+Ablates given parameter, run from command line 
+"""
+
+from xtrainer import * 
+import numpy as np
+import sys
+
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+    results_path = './hypertune_results/'
+
+    # get command line args 
+    model_arg, dataset_arg, tune_min, tune_max, tune_step = sys.argv[1:]
+
+    settings_dict = {
+        "batch_size": 128,
+        "num_epochs": 1,
+        "shuffle_train": True,
+        "flatten_dim": 3072,
+        "num_classes": 10,
+        "dataset": dataset_arg,
+        "model": model_arg,
+        "loss": 'CrossEntropyLoss',
+        "optimizers": ['KinFwd'],
+        "tune_param": 'g',
+        "tune_range": list(np.arange(float(tune_min), float(tune_max), float(tune_step)))
+    }
+
+    if model_arg == 'Linear':
+        if dataset_arg == 'MNIST':
+            settings_dict['flatten_dim'] = 784
+        elif dataset_arg == 'CIFAR10':
+            settings_dict['flatten_dim'] = 3072
+        else:
+            raise NotImplementedError
+        settings_dict['num_classes']
+    else:
+        settings_dict['flatten_dim'] = None
+
+    print(settings_dict)
+
+    # init datasets
+    train_dataset = getattr(torchvision.datasets, settings_dict['dataset'])(root='../../data', 
+                                               train=True, 
+                                               transform=transforms.ToTensor(), 
+                                               download=True)
+
+    test_dataset = getattr(torchvision.datasets, settings_dict['dataset'])(root='../../data', 
+                                              train=False, 
+                                              transform=transforms.ToTensor())
+
+    # init data loaders
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                               batch_size=settings_dict['batch_size'], 
+                                               shuffle=settings_dict['shuffle_train'])
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                              batch_size=settings_dict['batch_size'], 
+                                              shuffle=False)
+
+    # init loss criterion
+    criterion = getattr(torch.nn, settings_dict['loss'])()
+
+    # get model function 
+    if hasattr(torchvision.models, settings_dict['model']):
+        # torchvision model, e.g. resnet 18
+        model_attr = getattr(torchvision.models, settings_dict['model']) #call: ()
+    elif hasattr(torch.nn, settings_dict['model']):
+        # torch.nn layer, e.g. Linear
+        model_attr = getattr(torch.nn, settings_dict['model']) #call: (settings_dict['flatten_dim'], settings_dict['num_classes'])
+    else:
+        # model not implemented
+        raise NotImplementedError
+
+    # get optimizer function 
+    opt_name = settings_dict['optimizers'][0]
+    if hasattr(torch.optim, opt_name):
+        opt_attr = getattr(torch.optim, opt_name)
+    elif hasattr(kinematics, opt_name):
+        opt_attr = getattr(kinematics, opt_name)
+    else:
+        raise NotImplementedError
+
+    # iterate over tune parameter values
+    for tune_ind in range(len(settings_dict['tune_range'])):
+        tune_value = settings_dict['tune_range'][tune_ind]
+        print("TUNING {}".format(tune_value))
+        assert(int(tune_value) != 0) # for results filename
+        # init model
+        if settings_dict['flatten_dim'] == None:
+            model = model_attr()
+        else:
+            model = model_attr(settings_dict['flatten_dim'], settings_dict['num_classes'])
+        # init opt 
+        opt = opt_attr(model.parameters())
+        # set tuned value
+        param_to_tune = settings_dict['tune_param']
+        assert(hasattr(opt, param_to_tune))
+        setattr(opt, param_to_tune, tune_value)
+        # init trainer 
+        xTrainer = XTrainer([model], criterion, [opt], settings_dict['optimizers'], train_loader, test_loader, flatten_dim = settings_dict['flatten_dim'], num_epochs = settings_dict['num_epochs'])
+        # get results 
+        train_results, test_results = xTrainer.train()
+        # prepare results for pickling 
+        results_to_save = {opt_name: {'train_results': train_results, 'test_results': test_results, settings_dict['tune_param']: tune_value}}
+        results_to_save['settings'] = settings_dict
+        # pickle results 
+        pickle_filename = results_path + '{}_{}_{}_{}_{}_{}_E{}_B{}.pickle'.format(settings_dict['dataset'], settings_dict['model'], settings_dict['loss'], opt_name, settings_dict['tune_param'], int(tune_value), settings_dict['num_epochs'], settings_dict['batch_size'])
+        with open(pickle_filename, 'wb') as handle:
+            pickle.dump(results_to_save, handle, protocol = pickle.HIGHEST_PROTOCOL)
+
+        
+
+    print('initialized successfully')
+    print(settings_dict)
+
+    
+
+    
+    
+    
+    
