350cb1d3b0b8f78b6e75a13ccda765c84701dde0
Daniel Aaron <danjaaron@gmail.com>
2019-09-05 19:16:58 -0700

    adaptive g

--- kinematics.py
+++ kinematics.py
@@ -21,6 +21,7 @@ class KinFwd(Optimizer):
         self.grad_dict = {} 
         self.params = params
         self.model_save_name = './'+str(round(time.time()))
+        self.past_g = []
         
 
     def time_of_impact(self, uf_in):
@@ -47,7 +48,7 @@ class KinFwd(Optimizer):
             loss (req): PyTorch loss Tensor at each step 
         """
         # save model 
-        torch.save(model.state_dict(), self.model_save_name)
+        # torch.save(model.state_dict(), self.model_save_name)
 
         # get norm of total old gradient 
         old_grad_tens = torch.Tensor().to('cuda')
@@ -68,6 +69,7 @@ class KinFwd(Optimizer):
         old_h = float(closure().item())
 
         # get initial velocity (old_grad)
+        new_location = {}
         old_grad_dict = {}
         group_index = 0
         for group in self.param_groups:
@@ -81,13 +83,16 @@ class KinFwd(Optimizer):
                 old_grad_dict[group_index][param_index] = old_grad
                 # update position 
                 p.data.add_(-t_impact, old_grad) 
+                new_location[p] = p.data.clone()
                 param_index += 1
             group_index += 1
 
+        
         # reset gradients
         loss = closure()
-        loss.backward()
+        #loss.backward()
 
+        '''
         # get norm of total NEW gradient 
         new_grad_tens = torch.Tensor().to('cuda')
         for group in self.param_groups:
@@ -112,16 +117,43 @@ class KinFwd(Optimizer):
                 new_grad_dict[group_index][param_index] = new_grad
                 param_index += 1
             group_index += 1
-
+        '''
         new_h = float(closure().item())
 
-        # adjust initial time of flight (lands at new_h, not 0)
-        adjusted_vf = self.get_final_velocity(start_height = (old_h + old_h - new_h)) # adjust height either (old_h + new_h) or (old_h + (old_h - new_h))
-        t_impact = self.time_of_impact(adjusted_vf)
+        
 
+        """ Update g
+        """ 
+        # t_impact = math.sqrt(self.g/(2.*(abs(new_h - old_h))))
+        
+        #self.g = 2.*(old_h + old_h - new_h)/(t_impact**2)
+        #self.past_g.append(self.g)
+        # self.g = np.mean(self.past_g)
+        # print(self.g)
+
+        new_g = 2.*(old_h + old_h - new_h)/(t_impact**2)
+        if self.g == 9.8:
+            self.g = new_g
+        else:
+            self.g = (old_h/(new_h + old_h))*self.g + (new_h/(new_h+old_h))*new_g
+
+        # self.g = (self.g + 2.*(old_h + old_h - new_h)/(t_impact**2))/2. #self.vf/t_impact
+        
+        
+
+        # get new time 
+        
+
+        '''
+        # adjust initial time of flight (lands at new_h, not 0)
+        adjusted_vf = self.get_final_velocity(start_height = old_h + (old_h - new_h))#+ old_h - new_h)) # adjust height either (old_h + new_h) or (old_h + (old_h - new_h))
+        t_impact = self.time_of_impact(adjusted_vf)
+        print(t_impact)
+        '''
+        '''
         # restore model 
         model.load_state_dict(torch.load(self.model_save_name))
-
+        
         # choose averaged initial velocity 
         group_index = 0
         for group in self.param_groups:
@@ -129,14 +161,18 @@ class KinFwd(Optimizer):
             for p in group['params']:
                 if p.grad is None:
                     continue
-                new_grad = new_grad_dict[group_index][param_index]
+                #new_grad = new_grad_dict[group_index][param_index]
                 old_grad = old_grad_dict[group_index][param_index] 
-                avg_grad = (new_grad + old_grad)/2.0 
+                # avg_grad = (new_grad + old_grad)/2.0 
+                #avg_grad = (self.grad_dict[group_index][param_index] + p.grad)/2.0
                 # update position 
-                p.data.add_(-t_impact, avg_grad) 
+                #p.data.add_(-t_impact, avg_grad)
+                p.data = p.data + -t_impact*p.grad/old_grad_norm
+                # p.data = (p.data + new_location[p])/2.0 
+                # p.data = (p.data + (new_location[p] - p.data)/2.) #/2.
                 param_index += 1
             group_index += 1
 
         final_h = closure().item()
-
+        '''
         return loss
