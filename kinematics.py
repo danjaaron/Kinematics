32a551aeeff8ec7ff05286fed31f12bba635b126
Daniel Aaron <danjaaron@gmail.com>
2019-11-19 17:33:32 -0800

    refactor

---
+++ kinematics.py
@@ -0,0 +1,245 @@
+import torch
+from torch.optim import Optimizer
+import numpy as np 
+import math, time
+import copy
+from copy import deepcopy
+
+
+class Kin(Optimizer):
+
+    alias = 'K'
+
+    def __init__(self, params):
+        self.g = 1.0
+        default_dict = {'g': float(self.g)}
+        # print(super(self.__class__)
+        super(Kin, self).__init__(params, defaults = default_dict)
+
+    def __setstate__(self, state):
+        super(Kin, self).__setstate__(state)
+
+    def get_grad_norm(self):
+        """ Get norm of gradient
+        """
+        flat_grad = torch.Tensor().to('cuda')
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                else:
+                    flat_grad = torch.cat((flat_grad, p.grad.view(-1).to('cuda')))
+        grad_norm = torch.norm(flat_grad, p = 2)
+        return grad_norm
+
+
+    def get_step_size(self, loss):
+        return np.sqrt(2.0*loss/self.g)
+
+    def update_params(self, step_size, grad_norm):
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                p.data.add_(-step_size, p.grad / grad_norm)
+
+    def update_g(self, h0, hf):
+        if hf > h0:
+            self.g *= 2.0           
+
+    def step(self, closure, model):
+        """ Update parameters 
+        """ 
+
+        self.closure = closure
+
+        h0 = closure().item()
+
+        grad_norm = self.get_grad_norm()
+        step_size = self.get_step_size(h0)
+        self.update_params(step_size, grad_norm)
+
+        hf = closure().item()
+
+        self.update_g(h0, hf)
+
+        return h0
+
+
+
+class KinMnm(Kin):
+
+    alias = 'M'
+
+    def __init__(self, params):
+        super(KinMnm, self).__init__(params)
+        self.v = {}
+        self.b1 = 0.9
+        for (gidx, group) in enumerate(self.param_groups):
+                self.v[gidx] = dict()
+                for (pidx, p) in enumerate(group['params']):
+                    self.v[gidx][pidx] = dict()
+                    self.v[gidx][pidx]['m1'] = 0.
+                    self.v[gidx][pidx]['g'] = 1.0
+
+
+    def update_params(self, step_size, grad_norm):
+
+        # update params
+        for (gidx, group) in enumerate(self.param_groups):
+            for (pidx, p) in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                else:
+
+                    group = self.v[gidx][pidx]
+
+                    if self.t == 1:
+                        group['m1'] = torch.zeros_like(p.data)
+                        group['m2'] = torch.zeros_like(p.data)
+
+
+                    h0 = self.closure().item()
+
+                    t_flight = math.sqrt(2.0*h0/group['g'])
+
+                    p.data.add_(-t_flight*group['m1'])
+
+                    group['m1'] = (1 - self.b1)*group['m1'] + self.b1*(p.grad / grad_norm)
+
+                    hf = self.closure().item()
+
+                    if hf > h0:
+                        # p_norm = torch.norm(p.grad.view(-1), p = 2).item()
+                        group['g'] *= 2
+
+
+
+    def update_g(self, h0, hf):
+        if False:
+            self.g *= 2.0
+
+
+
+class KinAdg(Kin):
+    """ Kinematics with adaptive (per-group) g value
+    ... double g for each group if loss goes up after group is updated
+    """
+
+    alias = 'G'
+
+    def __init__(self, params):
+        super(KinAdg, self).__init__(params)
+        self.g_dict = {}
+        self.v = {}
+        self.beta = 0.8
+        for (gidx, group) in enumerate(self.param_groups):
+            self.g_dict[gidx] = dict()
+            self.v[gidx] = dict()
+            for (pidx, p) in enumerate(group['params']):
+                self.g_dict[gidx][pidx] = 1.0
+                self.v[gidx][pidx] = 0.0
+
+
+    def update_params(self, closure, grad_norm):
+        for (gidx, group) in enumerate(self.param_groups):
+            for (pidx, p) in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                h0 = closure().item()
+                step_size = np.sqrt(2.0 * h0 / self.g_dict[gidx][pidx]) # time 
+                dp = -step_size * p.grad / grad_norm
+                p.data.add_(dp)
+                hf = closure().item()
+
+                p_norm = torch.norm(p.grad.view(-1).to('cuda'), p = 2).item()
+                if hf > h0:
+                    self.g_dict[gidx][pidx] *= 2.
+               
+
+    def step(self, closure, model):
+        """ Update parameters 
+        """ 
+
+        h0 = closure().item()
+
+        grad_norm = self.get_grad_norm()
+        self.update_params(closure, grad_norm)
+
+        return h0
+
+
+
+class KinAdgP(KinAdg):
+
+    alias = 'P'
+
+    def update_params(self, closure, grad_norm):
+        for (gidx, group) in enumerate(self.param_groups):
+            for (pidx, p) in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                h0 = closure().item()
+                step_size = np.sqrt(2.0 * h0 / self.g_dict[gidx][pidx]) # time 
+                dp = -step_size * p.grad / grad_norm
+                p.data.add_(dp)
+                hf = closure().item()
+
+                p_norm = torch.norm(p.grad.view(-1).to('cuda'), p = 2).item()
+                if hf > h0:
+                    self.g_dict[gidx][pidx] += p_norm ** 2
+
+
+
+class KinOptg(KinAdg):
+
+    alias = 'O'
+
+    def update_params(self, closure, grad_norm):
+        for (gidx, group) in enumerate(self.param_groups):
+            for (pidx, p) in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                h0 = closure().item()
+                step_size = np.sqrt(2.0 * h0 / self.g_dict[gidx][pidx])
+                dp = -step_size * p.grad / grad_norm
+                p.data.add_(dp)
+                hf = closure().item()
+                if hf > h0:
+                    p.data.add_(-dp)
+                    orig_g = float(self.g_dict[gidx][pidx])
+                    glist = []
+                    for gstep in [-orig_g/10., orig_g/10.]:
+                        curr_dl = hf - h0 
+                        prev_dl = float('Inf')
+                        g = float(orig_g)
+                        i = 0
+                        while curr_dl < prev_dl:
+                            prev_dl = curr_dl
+                            g += gstep 
+                            if g == 0:
+                                break
+                            # get dl for gstep
+                            _h0 = closure().item()
+                            step_size = np.sqrt(2.0 * _h0 / g)
+                            dp = -step_size * p.grad / grad_norm
+                            p.data.add_(dp)
+                            _hf = closure().item()
+                            curr_dl = _hf - _h0=
+                            p.data.add_(-dp)
+                            i += 1
+                        # best g
+                        best_g = g - gstep #/ gstep
+                        glist.append((best_g, prev_dl))
+                    # take all time best g 
+                    g = min(glist, key = lambda x: x[1])[0]=
+                    # take step
+                    h0 = closure().item()
+                    step_size = np.sqrt(2.0 * h0 / g)
+                    dp = -step_size * p.grad / grad_norm
+                    p.data.add_(dp)
+                    hf = closure().item()
+                    dl = hf - h0
+                    # save g 
+                    self.g_dict[gidx][pidx] = g
+                       
---
+++ trainer.py
@@ -0,0 +1,179 @@
+"""
+xtrains kinematics models from kin_xopt
+"""
+
+# TODO: ability to use torch optimizers
+
+from functools import reduce 
+import operator 
+import argparse
+import torch, torchvision, time, inspect, pickle
+from torchvision import transforms
+import sys, os
+import kinematics
+import copy
+from copy import deepcopy
+
+class XTrainer:
+    """ Cross-trains EVERY optimization algorithm on the specific model type
+    """
+    def __init__(self, loss_criterion, model_list, optimizer_list, optimizer_names, train_loader, test_loader, num_epochs = 1, flatten_dim = None):
+        # global settings 
+        self.flatten_dim = flatten_dim
+        self.num_epochs = num_epochs
+        self.optimizer_names = optimizer_names
+        # device configuration
+        self.device = torch.device('cuda')
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        # init instance objects
+        self.model_list = [m.to(self.device) for m in model_list]
+        self.optimizer_list = optimizer_list
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.loss_criterion = loss_criterion
+        # get optimizer step parameters
+        self.optimizer_parameters = [list(dict(inspect.signature(o.step).parameters).keys()) for o in self.optimizer_list]
+        # init dicts to store results
+        self.train_results, self.test_results = {}, {}
+        for opt_ind in range(len(optimizer_list)):
+            self.train_results[opt_ind] = []
+            self.test_results[opt_ind] = []
+    
+    def train(self):
+        """ Trains and tests model according to epochs, storing test loss and training accuracy at each step
+        """
+        # set printing settings
+        total_step = len(self.train_loader)
+        self.print_every = 1 #int(round(total_step / 10.))
+        # train
+        for epoch in range(self.num_epochs):
+            for i, (data, target) in enumerate(self.train_loader): 
+                
+                # move all to device
+                data = data.to(self.device)
+                if self.flatten_dim != None:
+                    data = data.reshape(-1, self.flatten_dim).to(self.device)
+                target = target.to(self.device)
+
+                # iterate over optimizers and models
+                for opt_ind in range(len(self.optimizer_list)):
+                    # select opt and model
+                    self.optimizer = self.optimizer_list[opt_ind]
+                    self.model = self.model_list[opt_ind]
+                    # create closure function
+                    def closure():
+                        outputs = self.model(data)
+                        loss = self.loss_criterion(outputs, target)
+                        return loss
+                    # forward pass
+                    loss = closure()
+                    train_res, test_res = float(loss.item()), float(self.test())
+                    self.train_results[opt_ind].append(train_res)
+                    self.test_results[opt_ind].append(test_res)
+                    # backward pass
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    # optimize with appropriate arguments
+                    optimizer_parameters = self.optimizer_parameters[opt_ind]
+                    if optimizer_parameters == ['closure', 'model']:
+                        self.optimizer.step(closure, self.model)
+                    elif optimizer_parameters == ['closure']:
+                        self.optimizer.step(closure)
+                    else:
+                        self.optimizer.step()
+
+                # print results
+                if (i+1) % self.print_every == 0:
+                    # make a minidict for printing
+                    train_printdict = {self.optimizer_names[k]: v[-1] for k, v in self.train_results.items()}
+                    test_printdict = {self.optimizer_names[k]: v[-1] for k, v in self.test_results.items()}
+                    print(self.optimizer_names)
+                    print ('Epoch [{}/{}], Step [{}/{}], Loss: '.format(epoch+1, self.num_epochs, i+1, total_step) + str(train_printdict) + ', Val: ' + str(test_printdict))
+                           
+        return self.train_results, self.test_results
+
+    def test(self):
+        """ Returns test accuracy as a percentage over test loader
+        """
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for data, labels in self.test_loader:
+                # move all to device
+                data = data.to(self.device)
+                if self.flatten_dim != None:
+                    data = data.reshape(-1, self.flatten_dim).to(self.device)
+                labels = labels.to(self.device)
+                # get predictions
+                outputs = self.model(data)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                # score predictions
+                correct += (predicted == labels).sum().item()
+            # compute test accuracy percentage
+            test_accuracy = 100. * correct / total
+            return test_accuracy
+        
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description = 'Compare Kinematics algorithms')
+    parser.add_argument('kin_versions')
+    parser.add_argument('-d', '--dataset', default = 'MNIST')
+    parser.add_argument('-m', '--model', default = 'Linear')
+    parser.add_argument('-e', '--num_epochs', type = int, default = 1)
+    parser.add_argument('-b', '--batch_size', type = int, default = 128)
+    args = parser.parse_args()
+
+    # init loss criterion
+    criterion = getattr(torch.nn, 'CrossEntropyLoss')()
+
+    # get model, optimizer and data classes
+    model = [getattr(module, args.model) for module in [torch.nn, torchvision.models] if hasattr(module, args.model)][0]
+    kin_classes = [m[1] for m in inspect.getmembers(sys.modules['kinematics'], inspect.isclass) if hasattr(m[1], 'alias') and m[1].alias in args.kin_versions.upper()]
+    kin_classes.append(torch.optim.Adam)
+    dataset = getattr(torchvision.datasets, args.dataset)
+    assert(model and kin_classes)
+
+    # create dataloaders
+    train_dataset = dataset(root = './datasets/', train = True, download = True, transform=transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                            batch_size=args.batch_size, 
+                                            shuffle=True)
+    test_dataset = dataset(root = './datasets/', train = False, download = True, transform=transforms.ToTensor())
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                            batch_size=args.batch_size, 
+                                            shuffle=True)
+    num_classes = len(train_dataset.classes)
+
+    # create models
+    model_init = lambda x: x()
+    flatten_dim = None
+    if args.model == 'Linear':
+        in_features = reduce(operator.mul, train_dataset.data.shape[1:])
+        flatten_dim = in_features
+        model_init = lambda x: x(in_features, num_classes)
+    model_list = [model_init(model) for _ in kin_classes]
+
+    # create optimizers
+    opt_list = [k(model_list[k_idx].parameters()) for k_idx, k in enumerate(kin_classes)]
+    opt_names = [type(o).__name__ for o in opt_list]
+
+    # init trainer 
+    xTrainer = XTrainer(criterion, model_list, opt_list, opt_names, train_loader, test_loader, num_epochs = args.num_epochs, flatten_dim = flatten_dim)
+
+    # get results 
+    train_results, test_results = xTrainer.train()
+
+    # prepare results for pickling 
+    results_to_save = {opt_names[o]: {'train_results': train_results[o], 'test_results': test_results[o]} for o in range(len(model_list))}
+    results_to_save['args'] = args
+    
+    # pickle results 
+    pickle_path = './results/'
+    file_string = str(args.kin_versions)
+    pickle_filename = pickle_path + file_string + str(len([_ for _ in os.listdir(pickle_path) if file_string in _])) + '.pickle'
+    with open(pickle_filename, 'wb') as handle:
+        pickle.dump(results_to_save, handle, protocol = pickle.HIGHEST_PROTOCOL)
+
+    
